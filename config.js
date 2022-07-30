c04a4bba0cde83314107060e19fc83a967eac8f0
KelStore <107879165+KelStore@users.noreply.github.com>
2022-07-30 07:05:22 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+let config = {
+  ip: "20.214.136.38",
+  hostLink: "https://www.mediafire.com/file/imwof501id85kno/GTFI+3.96+v1.txt/file",
+  server_name: "GTFI", // Your server name
+  server_description: "GTFI ipsum dolor sit amet consectetur adipisicing elit. Sapiente qui unde consectetur at, beatae corporis officia delectus tempore asperiores dolores quia officiis quibusdam voluptas accusamus, fugit cum nulla, alias doloremque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum earum rerum fugiat aliquid eum quibusdam mollitia praesentium, fugit libero odit aperiam aliquam est quod nemo alias! Asperiores, earum ex! Reprehenderit?", // Your server description
+  about_server: "GTFI ipsum dolor sit amet consectetur adipisicing elit. Sapiente qui unde consectetur at, beatae corporis officia delectus tempore asperiores dolores quia officiis quibusdam voluptas accusamus, fugit cum nulla, alias doloremque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum earum rerum fugiat aliquid eum quibusdam mollitia praesentium, fugit libero odit aperiam aliquam est quod nemo alias! Asperiores, earum ex! Reprehenderit?", // About of your server
+  server_creator: [
+    {
+      name: "Kel#6666",
+      role: "Owner GTFI",
+    },
+    {
+      name: "Username#0000",
+      role: "Admin of the server",
+    },
+  ],
+  discord_server_link: "https://discord.gg/EVM2xBShWm",
+  server_images: [
+    // 2 image beside of your server description
+    "image/image1.jpg", // path to your image
+    "image/image1.jpg",
+  ],
+  partners: [], // Your server partners, ignore this if you don't have any server partners
+  features: [
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo facilis consequuntur repellendus cum possimus officiis tempore, rerum assumenda eaque accusantium illum alias saepe vitae dolor rem neque numquam sed!",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo facilis consequuntur repellendus cum possimus officiis tempore, rerum assumenda eaque accusantium illum alias saepe vitae dolor rem neque numquam sed!",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo facilis consequuntur repellendus cum possimus officiis tempore, rerum assumenda eaque accusantium illum alias saepe vitae dolor rem neque numquam sed!",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo facilis consequuntur repellendus cum possimus officiis tempore, rerum assumenda eaque accusantium illum alias saepe vitae dolor rem neque numquam sed!",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo facilis consequuntur repellendus cum possimus officiis tempore, rerum assumenda eaque accusantium illum alias saepe vitae dolor rem neque numquam sed!",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint explicabo facilis consequuntur repellendus cum possimus officiis tempore, rerum assumenda eaque accusantium illum alias saepe vitae dolor rem neque numquam sed!",
+  ],
+  galery: [
+    "image/image1.jpg", // path to your image
+    "image/image1.jpg", // path to your image
+    "image/image1.jpg", // path to your image
+    "image/image1.jpg", // path to your image
+  ],
+  
+};
